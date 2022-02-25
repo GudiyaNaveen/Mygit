28191736437304996fe491ae47b8781c787db23c
GudiyaNaveen <98573943+GudiyaNaveen@users.noreply.github.com>
2022-02-25 17:49:20 +0530

    Create toggle_nth_bit.c

---
+++ Assignments(25-02-2022)/toggle_nth_bit.c
@@ -0,0 +1,45 @@
+/* Write a C program to get and toggle nth bit of a number. */
+
+#include <stdio.h>
+#define TOGGLE_BIT(x) x^(0x1<<p)
+#define MY_SIZEOF(type) ((char *)(&type+1)-(char*)(&type))*8
+
+int main()
+{
+    int x,i,n,p,a;
+    printf("Enter a number: ");
+    scanf("%d",&x);
+    printf("Enter postion to clear a bit: ");
+    scanf("%d",&p);
+    n=MY_SIZEOF(x);
+    printf("before Toggle the %d bit:\n",p);
+    for(i=n-1;i>=0;i--)
+    {
+        if(x&(1<<i))
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    printf("\n");
+    a=TOGGLE_BIT(x);
+    printf("After Toggle the %d bit:\n",p);
+    for(i=n-1;i>=0;i--)
+    {
+        if(a&(1<<i))
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    printf("\n");
+    printf("%d\n",a);
+    return 0; 
+}
+
